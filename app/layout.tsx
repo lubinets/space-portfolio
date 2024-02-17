326ff669c41564fbe16064b90ba1d16e1ba8bf40
lubinets <lubineckarina@gmail.com>
2024-02-17 15:48:05 +0100

    Initial project commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Space Portfolio",
+  description: "My Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
